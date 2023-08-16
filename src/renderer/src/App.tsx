8ab7654a86bbc09ce17bfa2294a4d0cebc3d39ff
Irene Wang <ireneziwang@gmail.com>
2023-08-16 14:32:27 -0700

    fix application layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -9,10 +9,10 @@ function App(): JSX.Element {
     const [filePath, setFilePath] = useState(''); // may want to change this to some sort of redux, but useState is good for now i guess
     // dialog settings
     const dialogConfig = {
-    title: 'Select a project',
-    buttonLabel: 'Select',
-    properties: ['openDirectory']
-  }
+      title: 'Select a project',
+      buttonLabel: 'Select',
+      properties: ['openDirectory']
+    }
     // window.api.openDialog returns the filepath when the filepath is chosen from the dialog
   const openExplorer = async (): any => {
     const {filePaths} = await window.api.openDialog('showOpenDialog', dialogConfig)
@@ -22,13 +22,13 @@ function App(): JSX.Element {
   }
 
   return (
-    <div className="grid grid-rows-3 h-screen w-full">
-      {/* <div className="h-1/7"> */}
-        <Header onClick={openExplorer} projectName={filePath}/>
-      {/* </div> */}
-      {/* can we put this div inside of the tree */}
+    <div>
+      <Header onClick={openExplorer} projectName={filePath}/>
+      <div className="grid grid-rows-2 h-screen w-full">
         <Tree />
-      <div id="details-container" className="bg-blue-500">
+        <div id="details-container" className="bg-blue-500 ">
+          details
+        </div>
       </div>
     </div>
   )
