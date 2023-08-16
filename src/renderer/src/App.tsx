83342e612a0b36649c2c38d630198f609c88957e
Alisa Jin <alisaj831@gmail.com>
2023-08-16 10:31:19 -0700

    Add header with add directory button

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,11 @@
 // importing tailwind to use in the react components
 import 'tailwindcss/tailwind.css'
+import { useState } from 'react'
+import Header from './components/Header'
 
 function App(): JSX.Element {
+
+    const [filePath, setFilePath] = useState(''); // may want to change this to some sort of redux, but useState is good for now i guess
     // dialog settings
     const dialogConfig = {
     title: 'Select a project',
@@ -11,19 +15,19 @@ function App(): JSX.Element {
     // window.api.openDialog returns the filepath when the filepath is chosen from the dialog
   const openExplorer = async (): any => {
     const {filePaths} = await window.api.openDialog('showOpenDialog', dialogConfig)
+    const fileArray = filePaths[0].split('/')
+    setFilePath(fileArray[fileArray.length - 1]);
     console.log(filePaths[0]);  // returns an array, so indexed at 0 to retrieve path
   }
 
   return (
-    <div className="container mx-auto my-auto grid  grid-rows-3 grid-flow-col gap-4	">
+    <div className="container grid grid-rows-4 grid-flow-col gap-4 p-0 m-0">
+      <Header onClick={openExplorer} projectName={filePath}/>
       <header className="grid grid-cols-3	gap-4">
         <h1 className="bg-red-500">React-Relay</h1>
         <h4 id="current-app-name" className="bg-yellow-500">
           My-React-App
         </h4>
-        <button id="upload-button" className="open-app bg-violet-500" onClick={openExplorer}>
-          Open App File
-        </button>
       </header>
 
       <div id="tree-container" className="tree-container bg-green-500">
