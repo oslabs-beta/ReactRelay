a05c13944b8099caa792be99cdc9bf494e128659
Alisa Jin <alisaj831@gmail.com>
2023-08-17 18:10:26 -0700

    Add buttons to details component when a node on the reactFlow tree is clicked, currently method buttons are attached to a dummy function that console logs the object we want to send in a post request

--- src/renderer/src/components/Details.tsx
+++ src/renderer/src/components/Details.tsx
@@ -1,11 +1,23 @@
+import MethodButton from "./MethodButton"
+import { useState, useEffect } from "react";
 
-function Details(): JSX.Element  {
+function Details({ componentName, nodeInfo }): JSX.Element  {
+  const [methodButtons, setMethodButtons] = useState([]);
   // is this where we are gonna wanna start using redux, to keep track of file path state?
+
+  useEffect(() => {
+    const buttons = nodeInfo.map(nodeObj => <MethodButton methodName={nodeObj.method} endPointName={nodeObj.fullRoute}/>)
+    setMethodButtons(buttons);
+  }, [nodeInfo])
+
   return (
     <div>
-      Details
+      <h1 className='text-3xl font-bold ml-6 mb-4'>{componentName}</h1>
+      <div id='method-btn-container' className="flex flex-col gap-3 ml-5 w-fit">
+        {nodeInfo.length !== 0 && methodButtons}
+      </div>
     </div>
   )
 }
 
-export default Details
+export default Details;
