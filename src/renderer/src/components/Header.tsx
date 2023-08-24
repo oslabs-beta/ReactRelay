1e9e4b659399c7d5405a730943248f21747202c8
Irene Wang <ireneziwang@gmail.com>
2023-08-24 15:26:34 -0700

    change logo

--- src/renderer/src/components/Header.tsx
+++ src/renderer/src/components/Header.tsx
@@ -1,16 +1,19 @@
 import dirUpload from '../assets/images/directory-black.svg'
-import appLogo from '../assets/images/ReactRelay.svg'
+// import appLogo from '../assets/images/ReactRelay.svg'
+import appLogo from '../assets/images/ReactRelay-logos/ReactRelay-logos_white.png'
 
 function Header({openFileExplorer, projectName}) {
   return (
     <header className='flex justify-between p-8 bg-primary'>
-      <img className='object-none h-[45px] w-[440px]' src={appLogo}/>
+      <div>
+        <img className='object-none h-[45px] w-[440px]' src={appLogo}/>
+      </div>
       <div className='flex items-center'>
-        
+
         <p className='text-lg mx-2'>{projectName}</p>
-        <div className="tooltip tooltip-left" data-tip="Open a new project">  
+        <div className="tooltip tooltip-left" data-tip="Open a new project">
           <button className="btn" onClick={openFileExplorer}><img className='h-7' src={dirUpload} alt='open folder button'/></button>
-          
+
         </div>
 
         {/* <button className="btn bg-accent" onClick={openFileExplorer}><img className='h-7' src={dirUpload} alt='open folder button'/></button> */}
