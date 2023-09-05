80dad191b02e6c9e4f4ee06a08eb85249261feec
Nick Cushman <cushmann572@gmail.com>
2023-09-05 10:46:06 -0700

    begin process to add search functionality. currently implementing memo and useMemo in customNode component to avoid re-rendering all components on every key stroke

---
+++ src/renderer/src/features/searchSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  searchValue: '',
+};
+
+export const searchSlice = createSlice({
+  name: 'search',
+  initialState,
+  reducers: {
+    setSearchValue: (state, action) => {
+      state.searchValue = action.payload;
+    },
+  },
+});
+
+export const {
+  setSearchValue
+} = searchSlice.actions;
+export default searchSlice.reducer;
--- src/renderer/src/redux/store.js
+++ src/renderer/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import projectReducer from '../features/projectSlice';
 import detailReducer from '../features/detailSlice';
+import searchReducer from '../features/searchSlice';
 
 export const store = configureStore({
   reducer: {
     project: projectReducer,
     detail: detailReducer,
+    search: searchReducer,
   },
 });
