263a4dfcdc29cfa2e943656ca22ece654d344fcc
Irene Wang <ireneziwang@gmail.com>
2023-09-06 21:19:24 -0700

    update styling on search. remove project name. focus on text input code is present but isnt working

--- src/renderer/src/components/Header.tsx
+++ src/renderer/src/components/Header.tsx
@@ -1,8 +1,9 @@
+import { useRef } from 'react';
 import dirUpload from '../assets/images/directory-black.svg';
 // import appLogo from '../assets/images/ReactRelay.svg'
 import appLogo from '../assets/images/ReactRelay-logos/ReactRelay-logos_white.png';
 import { useDispatch, useSelector } from 'react-redux';
-import { setSearchValue , setShowSearch} from '../features/searchSlice';
+import { setSearchValue, setShowSearch } from '../features/searchSlice';
 
 
 function Header({ projectName }): JSX.Element {
@@ -10,9 +11,17 @@ function Header({ projectName }): JSX.Element {
   const searchBar = useSelector((state: any) => state.search.searchValue);
   const showSearch = useSelector((state: any) => state.search.showSearch);
 
-  // const handleSearch = () => {
+  const searchInputRef = useRef<HTMLInputElement | null>(null);
 
-  // }
+
+  const handleSearchClick = () => {
+
+    dispatch(setShowSearch());
+
+    if (searchInputRef.current) {
+      searchInputRef.current.focus();
+    }
+  }
 
   return (
     <header className='flex justify-between p-3 bg-primary'>
@@ -20,7 +29,7 @@ function Header({ projectName }): JSX.Element {
         <img className='object-cover h-[50px] ml-[-35px] w-full' src={appLogo} />
       </div>
 
-{/*
+      {/*
       <div className='flex items-center ml-[-70px]'>
       <h2>Component Search: </h2>
       <input id='componentSearch' onChange={(e) => dispatch(setSearchValue(e.target.value))} value={searchBar} type="text" />
@@ -29,10 +38,22 @@ function Header({ projectName }): JSX.Element {
 
       <div className='flex items-center'>
         {/* <p className='text-lg mx-2'>{projectName}</p> */}
-{/*
-        <button className="btn btn-ghost btn-circle" onClick = {()=>dispatch(setShowSearch())}>
+        {showSearch && (
+          <input
+            ref={searchInputRef}
+            id="search-input"
+            className="input border p-3 input-sm"
+            type="text"
+            id='componentSearch'
+            onChange={(e) => dispatch(setSearchValue(e.target.value))}
+            value={searchBar}
+            placeholder="Search components"  />
+        )}
+
+        <button className="btn btn-ghost mx-2" onClick={handleSearchClick}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-        </button> */}
+        </button>
+
 
         <div className="tooltip tooltip-left" data-tip="Open a new project">
           <button className="btn" onClick={() => window.openExplorerModal.showModal()}><img className='h-7' src={dirUpload} alt='open folder button' /></button>
